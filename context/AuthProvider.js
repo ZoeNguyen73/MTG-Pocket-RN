914346d137eb1f466cc1362406f338c7e582e69e
Zoe Nguyen <nganguyen0703@gmail.com>
2024-12-03 21:21:15 +0800

    feat: use the newly created axios.js in AuthProvider

--- context/AuthProvider.js
+++ context/AuthProvider.js
@@ -1,6 +1,7 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import axios from "axios";
+
+import axios from "../api/axios";
 
 const AuthContext = createContext({});
 
@@ -13,55 +14,104 @@ export const AuthProvider = ({ children }) => {
     username: "",
     accessToken: "",
     avatar: "",
-  })
-};
+  });
 
-const LogIn = async ({ username, hash }) => {
-  setIsLoading(true);
-
-  const response = await axios.post(
-    "/auth/login",
-    { username, hash },
-  );
-
-  const { accessToken, refreshToken, avatar } = response.data;
-
-  await AsyncStorage.setItem("username", username);
-  await AsyncStorage.setItem("accessToken", accessToken);
-  await AsyncStorage.setItem("refreshToken", refreshToken);
-  await AsyncStorage.setItem("avatar", avatar);
-
-  setAuth({ username, accessToken, avatar });
-  setIsLoggedIn(true);
-
-  setIsLoading(false);
-};
-
-const logOut = async () => {
-  setIsLoading(true);
-
-  try {
-    const refreshToken = await AsyncStorage.getItem("refreshToken");
-    await axios.delete(
-      "/auth/logout",
-      { data: { refreshToken }}
+  const logIn = async ({ username, hash }) => {
+    setIsLoading(true);
+  
+    const response = await axios.post(
+      "/auth/login",
+      { username, hash },
     );
-
-    await AsyncStorage.removeItem("username", username);
-    await AsyncStorage.removeItem("accessToken", accessToken);
-    await AsyncStorage.removeItem("refreshToken", refreshToken);
-    await AsyncStorage.removeItem("avatar", avatar);
-
-    setAuth({
-      username: "",
-      accessToken: "",
-      avatar: "",
-    });
-    setIsLoggedIn(false);
-
-  } catch (error) {
-    // handle error
-  } finally {
+  
+    const { accessToken, refreshToken, avatar } = response.data;
+  
+    await AsyncStorage.setItem("username", username);
+    await AsyncStorage.setItem("accessToken", accessToken);
+    await AsyncStorage.setItem("refreshToken", refreshToken);
+    await AsyncStorage.setItem("avatar", avatar);
+  
+    setAuth({ username, accessToken, avatar });
+    setIsLoggedIn(true);
+  
     setIsLoading(false);
-  }
+  };
+  
+  const logOut = async () => {
+    setIsLoading(true);
+  
+    try {
+      const refreshToken = await AsyncStorage.getItem("refreshToken");
+      await axios.delete(
+        "/auth/logout",
+        { data: { refreshToken }}
+      );
+  
+      await AsyncStorage.removeItem("username", username);
+      await AsyncStorage.removeItem("accessToken", accessToken);
+      await AsyncStorage.removeItem("refreshToken", refreshToken);
+      await AsyncStorage.removeItem("avatar", avatar);
+  
+      setAuth({
+        username: "",
+        accessToken: "",
+        avatar: "",
+      });
+      setIsLoggedIn(false);
+  
+    } catch (error) {
+      // handle error
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const getCurrentUser = async () => {
+    setIsLoading(true);
+
+    try {
+      const currentUsername = await AsyncStorage.getItem("username");
+      const currentAccessToken = await AsyncStorage.getItem("accessToken");
+      const currentRefreshToken = await AsyncStorage.getItem("refreshToken");
+      const currentAvatar = await AsyncStorage.getItem("avatar");
+  
+      if (!currentUsername || !currentAccessToken || !currentRefreshToken || !currentAvatar) {
+        setAuth({
+          username: "",
+          accessToken: "",
+          avatar: "",
+        });
+        setIsLoading(false);
+        setIsLoggedIn(false);
+        return;
+      }
+  
+      setAuth({
+        username: currentUsername,
+        accessToken: currentAccessToken,
+        avatar: currentAvatar,
+      });
+
+      setIsLoggedIn(true);
+
+    } catch (error) {
+      setIsLoggedIn(false);
+      //handle error
+
+    } finally {
+      setIsLoading(false;)
+    }
+
+    useEffect(() => {
+      getCurrentUser();
+    }, [])
+  };
+  
+  return (
+    <AuthContext.Provider value={{ logIn, logOut, setAuth, auth, isLoggedIn, setIsLoggedIn, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  )
+
 };
+
