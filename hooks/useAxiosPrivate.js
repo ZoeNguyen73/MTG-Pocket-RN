ab17a33c5bdb926968d0584e9c04527d0a2fb3cf
Zoe Nguyen <nganguyen0703@gmail.com>
2024-12-04 16:09:41 +0800

    feat: add basic structure to useAxiosPrivate and useRefreshToken

---
+++ hooks/useAxiosPrivate.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+import { axiosPrivate } from "../api/axios";
+import { useAuthContext } from "../context/AuthProvider";
+import useRefreshToken from "./useRefreshToken";
+
+const useAxiosPrivate = () => {
+
+};
+
+export default useAxiosPrivate;
---
+++ hooks/useRefreshToken.js
@@ -0,0 +1,12 @@
+import { useEffect, useRef } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { useAuthContext } from "../context/AuthProvider";
+import handleGlobalError from "../utils/ErrorHandler";
+import axios from "../api/axios";
+
+const useRefreshToken = () => {
+
+};
+
+export default useRefreshToken;
