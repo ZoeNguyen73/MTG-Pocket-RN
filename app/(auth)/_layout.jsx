065d413395604408f9199ce613ac26614da83814
Zoe Nguyen <nganguyen0703@gmail.com>
2024-12-09 14:08:50 +0800

    feat: set up basic structure for register.jsx

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+import { useThemeContext } from "../../context/ThemeProvider";
+import tailWindConfig from "../../tailwind.config";
+
+const AuthLayout = () => {
+  const { theme } = useThemeContext();
+
+  const lightBackgroundColor = tailwindConfig.theme.extend.colors.light.background;
+  const darkBackgroundColor = tailwindConfig.theme.extend.colors.dark.background;
+
+  return (
+    <>
+      <Stack>
+        <Stack.Screen 
+          name="register"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+
+      <StatusBar 
+        backgroundColor={`${ theme === "dark" ? darkBackgroundColor : lightBackgroundColor }`} 
+        style={`${ theme === "dark" ? "light" : "dark"}`}
+      />
+    </>
+  )
+};
+
+export default AuthLayout;
---
+++ app/(auth)/register.jsx
@@ -0,0 +1,53 @@
+import { View, ScrollView, Keyboard } from "react-native";
+import { useState } from "react";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { useThemeContext } from "../../context/ThemeProvider";
+import { useAuthContext } from "../../context/AuthProvider";
+import { useErrorHandler } from "../../context/ErrorHandlerProvider";
+
+import Text from "../../components/CustomText/CustomText";
+import Button from "../../components/CustomButton/CustomButton";
+import { fixCurrentParams } from "expo-router/build/fork/getPathFromState-forks";
+
+import axios from "../../api/axios";
+
+const Register = () => {
+  const { theme } = useThemeContext();
+  const { handleError } = useErrorHandler();
+
+  const { auth, logOut, isLoggedIn, isLoading } = useAuthContext();
+  
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formErrors, setFormErrors] = useState({});
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [activationToken, setActivationToken] = useState("");
+
+  retun (
+    <>
+      <SafeAreaView className="bg-light-background dark:bg-dark-background h-full">
+        <ScrollView>
+
+          <View
+            className="w-full justify-center px-8 my-6"
+          >
+            <Text>Sign up for free</Text>
+          </View>
+
+        </ScrollView>
+      </SafeAreaView>
+      { isSubmitting && (<LoadingSpinner />) }
+    </>
+  )
+
+};
+
+export default Register;
