8b114d102012aa131eb9baf85ed33572e2f1165f
Zoe Nguyen <nganguyen0703@gmail.com>
2024-12-12 16:33:12 +0800

    feat: add set images for play boosters

--- constants/index.js
+++ constants/index.js
@@ -2,5 +2,6 @@ import logos from "./logos";
 import icons from "./icons";
 import avatars from "./avatars";
 import images from "./images";
+import sets from "./sets";
 
-export { logos, icons, avatars, images };
+export { logos, icons, avatars, images, sets };
